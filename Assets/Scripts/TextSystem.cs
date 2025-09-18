b7f6fb9ee8e62f50112fed69572946d999059c4d
Dziroz <abcdanil@bk.ru>
2025-09-18 19:43:46 +0500

    1

--- Assets/Scripts/TextSystem.cs
+++ Assets/Scripts/TextSystem.cs
@@ -18,6 +18,6 @@ public class TextSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        text.text = messages[day.dayInt];
+        text.text = messages[FoodSystem.saveday];
     }
 }
